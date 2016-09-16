89964518eabe128a027ab744b2df00d6c33a4c85
Jefferson Raga Salunga <jefferson.salunga@yahoo.com>
2016-09-16 16:39:50 +0800

    fix

--- resources/views/frontend/layout/master.blade.php
+++ resources/views/frontend/layout/master.blade.php
@@ -6,8 +6,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>@yield("title", "Filigans Hotel")</title>
 	<!-- Bootstrap CSS -->
-	
-	<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+
+	<link rel="shortcut icon" type="image/x-icon" href="/images/favicon-icon.png" />
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 	<!-- Optional theme -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
